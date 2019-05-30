36ad5782496f016b7886e4872859cbd5d6fa69da
vickyrr24 <51054175+vickyrr24@users.noreply.github.com>
2019-05-30 21:23:24 +0530

    Create roman.py

---
+++ roman.py
@@ -0,0 +1,12 @@
+#roman numbers
+val = [1000, 900, 500, 400,100, 90, 50, 40,10, 9, 5, 4,1]
+syb = ["M", "CM", "D", "CD","C", "XC", "L", "XL","X", "IX", "V", "IV","I"]
+n=int(input())
+roman=""
+i=0
+while(n>0):
+    for _ in range(n//val[i]):
+        roman+=syb[i]
+        n-=val[i]
+    i+=1
+print(roman)
