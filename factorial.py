20eaa72eb4445846ac487b0d8244cf9c4d1e13f8
vickyrr24 <51054175+vickyrr24@users.noreply.github.com>
2019-05-30 21:05:57 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,6 +1,8 @@
 num=int(input())
-n=1
-while num>0:
-	n=n*num
-	num=num-1
-print(n)
+def fact(num):
+    n=1
+    while num>0:
+            n=n*num
+            num=num-1
+    return(n)
+print(fact(num))
