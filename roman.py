875d81ee98a1ab4d1b6e5d939f7f8fc7cc052d29
vickyrr24 <51054175+vickyrr24@users.noreply.github.com>
2019-05-30 21:28:15 +0530

    Update roman.py

--- roman.py
+++ roman.py
@@ -1,12 +1,11 @@
-#roman numbers
-val = [1000, 900, 500, 400,100, 90, 50, 40,10, 9, 5, 4,1]
-syb = ["M", "CM", "D", "CD","C", "XC", "L", "XL","X", "IX", "V", "IV","I"]
-n=int(input())
-roman=""
-i=0
-while(n>0):
-    for _ in range(n//val[i]):
-        roman+=syb[i]
-        n-=val[i]
-    i+=1
-print(roman)
+def from_roman(num):
+    roman_numerals = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    result = 0
+    for i,c in enumerate(num):
+        if (i+1) == len(num) or roman_numerals[c] >= roman_numerals[num[i+1]]:
+            result += roman_numerals[c]
+        else:
+            result -= roman_numerals[c]
+    return result
+n=input()
+print(from_roman(n))
