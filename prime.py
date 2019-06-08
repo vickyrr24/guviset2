57c32370e93e0dabb36d7632e47becefb1521355
vickyrr24 <51054175+vickyrr24@users.noreply.github.com>
2019-06-08 21:06:18 +0530

    Create prime.py

---
+++ prime.py
@@ -0,0 +1,13 @@
+m,n=[int(x) for x in input().split()]
+prime=[True for x in range(n+1)]
+for i in range(2,n+1):
+    if(prime[i]==True):
+        temp=i*2
+        count=2
+        while(temp<n):
+            prime[temp]=False
+            temp=i*count
+            count+=1
+for i in range(m,n):
+    if(prime[i]==True):
+        print(i,end=" ")
