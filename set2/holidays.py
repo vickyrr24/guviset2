52cdd37b3fbdf44a522d187575bba1e709ebe44e
vickyrr24 <51054175+vickyrr24@users.noreply.github.com>
2019-06-10 18:03:15 +0530

    Update holidays.py

--- set2/holidays.py
+++ set2/holidays.py
@@ -1,3 +1,9 @@
 string=input()
-string=string.lower()
-print(string)
+lst=["monday","tuesday","wednesday","thursday","friday","saturday","sunday"]
+if(string.lower() in lst):
+    if(string.lower()=="sunday" or string.lower()=="saturday"):
+        print("yes")
+    else:
+        print("no")
+else:
+    print("Invalid")
