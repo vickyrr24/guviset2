fa0957114ed45504c092a0cf3c4d19e3f14e6ed4
vickyrr24 <51054175+vickyrr24@users.noreply.github.com>
2019-06-10 18:53:56 +0530

    Update cyclic.py

--- set2/cyclic.py
+++ set2/cyclic.py
@@ -1,5 +1,6 @@
 n,m=[int(x) for x in input().split()]
 lst=[int(x) for x in input().split()]
-for i in range(m):
-    lst[0],lst[len(lst)-1]=lst[len(lst)-1],lst[0]
-print(*lst)
+m=m%n
+temp=lst[-m:]+lst[:-m]
+print(temp)
+    
