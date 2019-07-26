497c5ab540f510da6a4ee06e65ea97fbc88b6fb1
vickyrr24 <51054175+vickyrr24@users.noreply.github.com>
2019-07-26 19:10:17 +0530

    Update new.py

--- pro/new.py
+++ pro/new.py
@@ -1,1 +1,26 @@
-
+n=int(input())
+lst=[]
+ma=""
+t=""
+for i in range(0,n):
+    temp = input()
+    lst.append(temp)
+for i in range(0,n-1):
+    st1=lst[i]
+    st2=lst[i+1]
+    if(len(st1)>len(st2)):
+        st1,st2=st2,st1
+    for i in range(len(st1)):
+        if(st1[i]==st2[i]):
+            t +=st1[i]
+        else:
+            break
+    if(len(ma)<len(t)):
+        ma=""
+        ma=t
+    t=""
+print(ma)
+            
+    
+        
+    
